4fc4c85cb04843ab2aa9e891d39da7750cd4d145
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-11-12 13:31:50 -0500

    installed inquirer and tested out the code with working results

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Triangle, Square, Circle } = require("./Develop/lib/shapes");
+const { Triangle, Square, Circle } = require("./Develop/examples/li/shapes");
 
 function writeToFile(fileName, answers) {
   let svgString = "";
